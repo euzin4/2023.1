73b17c05a66d5daf25ef4f50fd51b06c1bfaad52
kevin willian <kevin_willian@hotmail.com.br>
2023-05-19 16:01:19 -0300

    Update rubd.py

--- BD2/undoredo/rubd.py
+++ BD2/undoredo/rubd.py
@@ -1,5 +1,7 @@
 import json
 
-with open("/home/aluno/Imagens/redoundo/metadado.json") as dadosi:
-    data = json.load(dadosi)
-    print(data["INITIAL"])
+with open("/home/aluno/Imagens/undoredo/metadado.json") as dadosini:    #le todo o arquivo em "dadosini"
+    dicionario = json.load(dadosini)    #salva os dados em um formato manipulavel
+    print(dicionario["INITIAL"])
+    a1=dicionario["INITIAL"]["A"][0]    #salva um dado especifico em uma variavel
+    print(a1)
