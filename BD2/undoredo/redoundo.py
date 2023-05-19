babb7bef28b6dc26fc1f2f551794d5242a0ba30e
kevin willian <kevin_willian@hotmail.com.br>
2023-05-19 16:05:46 -0300

    Update redoundo.py

--- BD2/undoredo/redoundo.py
+++ BD2/undoredo/redoundo.py
@@ -3,5 +3,10 @@ import json
 with open("/home/aluno/Imagens/undoredo/metadado.json") as dadosini:    #le todo o arquivo em "dadosini"
     dicionario = json.load(dadosini)    #salva os dados em um formato manipulavel
     print(dicionario["INITIAL"])
-    a1=dicionario["INITIAL"]["A"][0]    #salva um dado especifico em uma variavel
-    print(a1)
+    a0=dicionario["INITIAL"]["A"][0]    #salva um dado especifico em uma variavel
+    a1=dicionario["INITIAL"]["A"][1]
+    b0=dicionario["INITIAL"]["B"][0]
+    b1=dicionario["INITIAL"]["B"][1]
+    #print(a0,a1)
+    #print(b0,b1)
+
